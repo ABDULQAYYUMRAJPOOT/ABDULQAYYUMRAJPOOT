13f8a4347c72c034c923a6ebf71fa58597bdace8
ABDULQAYYUMRAJPOOT <120580208+ABDULQAYYUMRAJPOOT@users.noreply.github.com>
2022-12-21 10:39:17 +0500

    Add files via upload
    these files are public

---
+++ animal.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+main(){
+cout << "  ----------------------------------------------   "<< endl ;
+cout << "                O       ^__^                             "<< endl ;
+cout << "                  O     (OO)\\__________ "<< endl << endl;
+cout << "                        (__)\\          )\\//\\ "<< endl;
+cout << "                             ||------W  |"<< endl;
+cout << "                             ||        || "<< endl;
+
+
+
+}
---
+++ rectangle.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+main(){
+cout << "******************************" << endl;
+cout << "*			     *" << endl;
+cout << "*			     *" << endl;
+cout << "*	QAYYUM'S 	     *" << endl;
+cout << "*    SOFTWARE HOUSE	     *" << endl;
+cout << "*			     *" << endl;
+cout << "*			     *" << endl;
+cout << "*			     *" << endl;
+cout << "******************************" << endl;}
